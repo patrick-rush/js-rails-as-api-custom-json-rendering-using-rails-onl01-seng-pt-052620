2940f14a935f091d328d9665c18680d3ce4b090b
patrick-rush <patrick.a.rush@gmail.com>
2020-12-07 21:10:29 -0500

    commit tree c122cc7eb18f63494438c8a404f9a4a2568e2d5c

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,16 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # render json: birds, only: [:id, :name, :species]
+    render json: birds, except: [:created_at, :updated_at]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: {id: bird.id, name: bird.name, species: bird.species}
+    if bird 
+      render json: bird.slice(:id, :name, :species)
+    else
+      render json: {message: 'Bird not found'}
   end
 end
